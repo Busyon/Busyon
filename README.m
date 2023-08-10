57a16f653786874b4f2a9d3aac0518c9434bcd7f
Busyon <141969256+Busyon@users.noreply.github.com>
2023-08-11 00:45:34 +0100

    Create README.m
    mbietiowo46@gmail.com

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Busyon
+- 👀 I’m interested in ... PROJECT MANAGEMENT
+- 🌱 I’m currently learning ... PROJECT MANAGEMENT
+- 💞️ I’m looking to collaborate on ... OUT TEAM
+- 📫 How to reach me ... VIA EMAIL mbietiowo46@gmail.com
+
+<!---
+Busyon/Busyon is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
